2946952ef4f15f670e4a147e2dc07ed145418775
frisk038 <frisktrentehuit@gmail.com>
2022-03-08 22:49:49 +0100

    Gameboard conponent

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Head from "./components/Head.vue";
 import Keyboard from "./components/Keyboard.vue";
+import Gameboard from "./components/Gameboard.vue";
 </script>
 
 <template>
@@ -11,14 +12,22 @@ import Keyboard from "./components/Keyboard.vue";
   <main></main>
 
   <footer>
+    <Gameboard :nbFail="nbFail"></Gameboard>
     <Keyboard @typed="typed"></Keyboard>
   </footer>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      secretWord: "maison",
+      nbFail: 0
+    }
+  },
   methods: {
     typed(key) {
+      this.nbFail++;
       console.log(key)
     }
   }
---
+++ src/components/Gameboard.vue
@@ -0,0 +1,60 @@
+<template>
+    <span>{{ smileyState }}</span>
+    <br />
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    props: ['nbFail'],
+    computed: {
+        smileyState() {
+            switch (this.nbFail) {
+                case 0:
+
+                    return '😄 _ _ _ _ _ _ _ _ _ _ 🐍';
+                    break;
+                case 1:
+                    return '  😅 _ _ _ _ _ _ _ _ _ 🐍';
+                    break;
+                case 2:
+                    return '    🙂 _ _ _ _ _ _ _ _ 🐍';
+                    break;
+                case 3:
+                    return '      🥲 _ _ _ _ _ _ _ 🐍';
+                    break;
+                case 4:
+                    return '        😐 _ _ _ _ _ _ 🐍';
+                    break;
+                case 5:
+                    return '          🙁 _ _ _ _ _ 🐍';
+                    break;
+                case 6:
+                    return '            😦 _ _ _ _ 🐍';
+                    break;
+                case 7:
+                    return '              😰 _ _ _ 🐍';
+                    break;
+                case 8:
+                    return '                😱 _ _ 🐍';
+                    break;
+                case 9:
+                    return '                  😖 _ 🐍';
+                    break;
+                case 10:
+                    return '                       💀';
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+</style>
